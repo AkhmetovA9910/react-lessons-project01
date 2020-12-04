dcd4035e3dd9a164be60a899c09562e805235eba
akhmetov9910 <ahmetovgame@gmail.com>
2020-12-04 20:23:53 +0600

    Reducers were some modified. File reduxStore.js was created

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -2,21 +2,26 @@ const SEND_MESSAGE = "SEND-MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
 const dialogsReducer = (state, action) => {
-   if (action.type === SEND_MESSAGE) {
-      let messageText = state.newMessageText;
-      if (messageText !== "" && messageText !== undefined && messageText !== null) {
-         state.messages.push({
-            owner: "me",
-            text: messageText
-         });
-         state.newMessageText = "";
-      }
+   switch (action.type) {
+      case SEND_MESSAGE:
+         let messageText = state.newMessageText;
+         if (messageText !== "" && messageText !== undefined && messageText !== null) {
+            state.messages.push({
+               owner: "me",
+               text: messageText
+            });
+            state.newMessageText = "";
+         }
+         return state;
+         break;
+      case UPDATE_NEW_MESSAGE_TEXT:
+         state.newMessageText = action.newMessageText;
+         return state;
+         break;
+      default:
+         return state;
+         break;
    }
-   else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-      state.newMessageText = action.newMessageText;
-   }
-
-   return state;
 };
 
 export default dialogsReducer;
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -2,23 +2,30 @@ const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
 const profileReducer = (state, action) => {
-   if (action.type === ADD_POST) {
-      let postText = state.newPostText;
-      if (postText !== "" && postText !== undefined && postText !== null) {
-         state.posts.push({
-            id: state.posts[state.posts.length - 1].id + 1,
-            text: postText,
-            likes: 0,
-            dislikes: 0
-         });
-         state.newPostText = "";
-      }
-   }
-   else if (action.type === UPDATE_NEW_POST_TEXT) {
-      state.newPostText = action.newText;
-   }
+   switch (action.type) {
+      case ADD_POST:
+         let postText = state.newPostText;
+         if (postText !== "" && postText !== undefined && postText !== null) {
+            state.posts.push({
+               id: state.posts[state.posts.length - 1].id + 1,
+               text: postText,
+               likes: 0,
+               dislikes: 0
+            });
+            state.newPostText = "";
+         }
+         return state;
+         break;
 
-   return state;
+      case UPDATE_NEW_POST_TEXT:
+         state.newPostText = action.newText;
+         return state;
+         break;
+
+      default:
+         return state;
+         break;
+   }
 };
 
 export default profileReducer;
